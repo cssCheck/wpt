6c1bb95dbc048a7c22e0ea7c520fd95873365245
Stephen McGruer <smcgruer@chromium.org>
2020-06-03 17:52:24 -0400

    Try making hosts?

--- tools/ci/ci_tools_unittest.sh
+++ tools/ci/ci_tools_unittest.sh
@@ -13,6 +13,8 @@ run_applicable_tox () {
     unset TOXENV
     local RUN_ENVS=$(tox -l | grep "^${OLD_TOXENV}\(\-\|\$\)" | tr "\n" ",")
     if [[ -n "$RUN_ENVS" ]]; then
+        # Hack.
+        ../wpt make-hosts-file | sudo tee -a /etc/hosts
         tox -e "$RUN_ENVS" wave/tests/test_wave.py || echo "failed, ignoring"
         cat /tmp/log.txt
     fi
